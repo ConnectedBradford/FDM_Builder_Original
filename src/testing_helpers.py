c89206eaf9ceda0ff70e2585887273ccfdbcc8df
samrelins <samrelins@gmail.com>
2022-02-14 11:07:15 +0000

    small updates to testing script

--- src/testing_helpers.py
+++ src/testing_helpers.py
@@ -17,21 +17,23 @@ def generate_random_dates(n=1, decade=1990):
          np.random.choice(range(1,29), (n,1))), 
         axis=1
     )
-    return [datetime.date(year=year, month=month, day=day)
-            for year, month, day in dates]
+    return pd.Series([datetime.date(year=year, month=month, day=day) 
+                     for year, month, day in dates],
+                     dtype="datetime64[ns]")
+#     return pd.Series([f"{year}-{month}-{day}" 
+#                       for year, month, day in dates],
+#                      dtype="string")
 
 
 def build_test_master_person_df():
     person_df = pd.DataFrame(dict(
         person_id = list(range(100)),
         birth_datetime = generate_random_dates(n=100),
-        death_datetime = np.repeat(None, 100)
+        death_datetime = pd.Series([], dtype="datetime64[ns]")
     ))
-    person_df["birth_datetime"] = person_df.birth_datetime.astype("datetime64")
     person_df.loc[person_df.person_id % 5 == 0, "death_datetime"] = (
-        generate_random_dates(n=20, decade=2010)
+        generate_random_dates(n=100, decade=2010)
     )
-    person_df["death_datetime"] = person_df.death_datetime.astype("datetime64")
     return person_df
 
 
@@ -82,6 +84,8 @@ def build_test_environment():
     master_person_table_id = f"{master_dataset_id}.person"
     master_person_df.to_gbq(destination_table=master_person_table_id,
                             project_id=PROJECT,
+                            table_schema=[{"name":"birth_datetime", "type": "DATE"},
+                                          {"name":"death_datetime", "type": "DATE"}],
                             progress_bar=None)
     
     demographics_df = build_test_demographics_df()
@@ -92,30 +96,47 @@ def build_test_environment():
     
     src_table_1 = demographics_df.iloc[:20,:]
     src_table_1.drop(["digest", "EDRN"], axis=1, inplace=True)
+    src_table_1 = add_junk_ids(src_table_1)
+    src_table_1["date"] = pd.Series([], dtype="datetime64[ns]")
+    src_table_1.iloc[:5,-1] = generate_random_dates(n=5, decade=1980)
+    src_table_1.iloc[5:15,-1] = generate_random_dates(n=10, decade=2000)
+    src_table_1.iloc[15:20,-1] = generate_random_dates(n=5, decade=2020)
+    src_table_1.iloc[20:25,-1] = generate_random_dates(n=5, decade=2000)
+    destination_table=f"{src_dataset_id}.src_table_1"
+    src_table_1.to_gbq(destination_table=destination_table, 
+                       progress_bar=None, 
+                       if_exists="replace", 
+                       table_schema=[{"name":"date", "type": "DATE"}], 
+                       project_id=PROJECT)
 
     src_table_2 = demographics_df.iloc[10:30,:]
     src_table_2.drop(["person_id", "EDRN"], axis=1, inplace=True)
+    src_table_2 = add_junk_ids(src_table_2)
 
     src_table_3 = demographics_df.iloc[20:40,:]
     src_table_3.drop(["person_id", "digest"], axis=1, inplace=True)
+    src_table_3 = add_junk_ids(src_table_3)
 
     src_table_4 = demographics_df.iloc[30:50,:]
     src_table_4.drop(["EDRN"], axis=1, inplace=True)
+    src_table_4 = add_junk_ids(src_table_4)
 
     src_table_5 = demographics_df.iloc[40:60,:]
     src_table_5.drop(["digest"], axis=1, inplace=True)
+    src_table_5 = add_junk_ids(src_table_5)
 
     src_table_6 = demographics_df.iloc[50:70,:]
     src_table_6.drop(["person_id"], axis=1, inplace=True)
+    src_table_6 = add_junk_ids(src_table_6)
 
-    all_src_tables = [src_table_1, src_table_2, src_table_3, 
+    all_src_tables = [src_table_2, src_table_3, 
                       src_table_4, src_table_5, src_table_6]
 
     for idx, table in enumerate(all_src_tables):
-        table["data"] = [str(i) for i in np.random.rand(20,)]
-        table = add_junk_ids(table)
-        destination_table=f"{src_dataset_id}.src_table_{idx+1}"
+        destination_table=f"{src_dataset_id}.src_table_{idx+2}"
         table.to_gbq(destination_table=destination_table,
                      progress_bar=None,
                      if_exists="replace",
                      project_id=PROJECT)
+        
+    
