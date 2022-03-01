b74f7545794509da0d45a1991907e769fd5eb913
Sam Relins <sam.relins@bthft.nhs.uk>
2022-03-01 13:31:23 +0000

    added dependencies to setup.py

--- setup.py
+++ setup.py
@@ -4,6 +4,8 @@ setup(
     name="FDMBuilder",
     packages=find_packages(),
     version="0.1.0",
+    install_requires=["google-cloud-bigquery", "pandas", "numpy", 
+                      "python-dateutil", "pandas-gbq"]
     description="Tools to build FDM Datasets for CYP",
     author="Sam Relins",
     licence="MIT"
